9092230247062b9bcbc54b32e46f300d8a17893f
cgao_ksm <137011200+ctgao@users.noreply.github.com>
2023-11-06 13:34:43 -0500

    Fixed a minor issue and added in some more comments

--- src/fetchBreedList.ts
+++ src/fetchBreedList.ts
@@ -6,8 +6,14 @@ const fetchBreedList: QueryFunction<
   ["breeds", Animal]
 > = async ({ queryKey }) => {
   const type = queryKey[0];
-  // no if-statement type checking necessary since typescript will not let you compile!
   const animal = queryKey[1];
+
+  // no if-statement type checking necessary since typescript will not let you compile!
+  // THE ABOVE STATEMENT IS A LIE!!! I GOT ERRORS!!!
+  // Why? because when animal is first declared and passed into this query, animal is "" meaning that all
+  // API calls return 400 (Bad Request). We need this statement to REFUSE the fetch statement when animal
+  // doesn't have a value even though it's considered "valid" since we did some typechecking earlier
+  if(!animal) return [];
 
   const apiRes = await fetch(
     `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
