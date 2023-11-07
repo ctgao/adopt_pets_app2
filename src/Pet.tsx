3a8dfc8b34957eecf3e536b5c2273b3a288d5fed
cgao_ksm <137011200+ctgao@users.noreply.github.com>
2023-11-07 16:41:06 -0500

    our first tests, passed!

--- src/Pet.tsx
+++ src/Pet.tsx
@@ -18,14 +18,14 @@ const Pet = (props: IProps) => {
   const { name, animal, breed, images, location, id } = props;
 
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-  if (images.length) {
+  if (images && images.length) {
     hero = images[0];
   }
 
   return (
     <Link to={`details/${id}`} className="relative block h-fit">
       <div className="float-left my-0 ml-5 mr-2.5">
-        <img src={hero} alt={name} />
+        <img data-testid="thumbnail" src={hero} alt={name} />
       </div>
       <div className="absolute bottom-0 left-5 bg-gradient-to-tr from-white to-transparent pr-2 pt-1 pl-1.5 pb-0.5">
         <h1>{name}</h1>
