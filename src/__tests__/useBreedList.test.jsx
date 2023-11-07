c31c1f281c3e9ff400fc69cbfb495968f242df84
cgao_ksm <137011200+ctgao@users.noreply.github.com>
2023-11-07 17:14:33 -0500

    another test down! and done two different ways :O

--- src/__tests__/useBreedList.test.jsx
+++ src/__tests__/useBreedList.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../oldUseBreedList";
 
@@ -13,28 +13,40 @@ const queryClient = new QueryClient({
   },
 });
 
-function getBreedList(animal) {
-  let list;
+// all of this was a hassle, let's start from SCRATCH!
+// function getBreedList(animal) {
+//   let list;
+//   // our fake test React component
+//   function TestComponent() {
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+//     list = useBreedList(animal);
+//     return null;
+//   }
+//
+//   render(
+//     <QueryClientProvider client={queryClient}>
+//       <TestComponent />
+//     </QueryClientProvider>
+//   );
+//   return list;
+// }
+//
+// test("gives an empty list with no animal provided", () => {
+//   const [breedList, status] = getBreedList();
+//
+//   expect(breedList).toHaveLength(0);
+//   expect(status).toBe("loading");
+// });
 
-  // our fake test React component
-  function TestComponent() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    list = useBreedList(animal);
-    return null;
-  }
+test("gives an empty list with no animal provided", () => {
+  const { result } = renderHook(() => useBreedList(""), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
 
-  render(
-    <QueryClientProvider client={queryClient}>
-      <TestComponent />
-    </QueryClientProvider>
-  );
+  const [breedList, status] = result.current;
 
-  return list;
-}
-
-// eslint-disable-next-line @typescript-eslint/require-await
-test("gives an empty list with no animal provided", async () => {
-  const [breedList, status] = getBreedList();
   expect(breedList).toHaveLength(0);
   expect(status).toBe("loading");
 });
